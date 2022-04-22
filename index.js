aa3f802e72e2c53db1124525cd0294bbd4d893e0
RitaM5 <sporshia108514orva@gmail.com>
2022-04-22 13:26:50 +0600

    params use

--- index.js
+++ index.js
@@ -6,15 +6,43 @@ app.get('/', (req, res) =>{
     res.send('hello from my smarty smarty pant!!')
 });
 
-
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: '0178888888' },
+    { id: 2, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '0178888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '0178888888' },
+    { id: 4, name: 'suchonda', email: 'suchonda@gmail.com', phone: '0178888888' },
+    { id: 5, name: 'srabonti', email: 'srabonti@gmail.com', phone: '0178888888' },
+    { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '0178888888' },
+    { id: 7, name: 'sohana', email: 'sohana@gmail.com', phone: '0178888888' },
+]
 
 
 app.get('/users', (req, res)=>{
-    res.send({id: 1, name: 'abdul halim', job:'khay sudhu halim'});
+    res.send(users);
 });
 
+app.get('/user/:id', (req, res)=>{
+   // console.log(req.params);
+    /* const id = req.params.id;
+    const user = users[id]; */
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user)
+   /*  if(!user){
+        res.send({"error": "user not found"});
+    }
+   else{ 
+       res.send(user)
+    } */
+});
 
+app.get('/fruits', (req, res) =>{
+    res.send(['mango', 'apple', 'oranges']);
+});
 
+app.get('/fruits/mango/fazle', (req, res) =>{
+    res.send('sour sour fazle flavor');
+});
 
 
 
