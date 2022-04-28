45d0079d23a256c96ed32d41ce15a70555aa255f
RitaM5 <sporshia108514orva@gmail.com>
2022-04-28 17:53:21 +0600

    code added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>{
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
     res.send('hello from my smarty smarty pant!!')
 });
 
@@ -15,37 +19,65 @@ const users = [
     { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '0178888888' },
     { id: 7, name: 'sohana', email: 'sohana@gmail.com', phone: '0178888888' },
 ]
-
-
-app.get('/users', (req, res)=>{
-    res.send(users);
+//app.get dara data read kora hoy
+app.get('/users', (req, res) => {
+    //filter by search quary parameter
+    if(req.query.name){
+       // console.log(req.query);
+       const search = req.query.name.toLowerCase();
+       const matched = users.filter(user => user.name.toLowerCase().includes(search))
+       res.json(matched)
+    }
+   else{ 
+       res.send(users);
+    }
 });
 
-app.get('/user/:id', (req, res)=>{
-   // console.log(req.params);
-    /* const id = req.params.id;
+app.get('/user/:id', (req, res) => {
+    // console.log(req.params);
+     /*const id = req.params.id;
     const user = users[id]; */
     const id = parseInt(req.params.id);
     const user = users.find(u => u.id === id);
     res.send(user)
-   /*  if(!user){
-        res.send({"error": "user not found"});
-    }
-   else{ 
-       res.send(user)
-    } */
 });
 
-app.get('/fruits', (req, res) =>{
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user) ;
+    res.json(user);
+});
+
+app.get('/fruits', (req, res) => {
     res.send(['mango', 'apple', 'oranges']);
 });
 
-app.get('/fruits/mango/fazle', (req, res) =>{
+app.get('/fruits/mango/fazle', (req, res) => {
     res.send('sour sour fazle flavor');
 });
 
-
-
 app.listen(port, () => {
     console.log('listening to port', port);
+}) 
+/* const express = require('express')
+const cors = require('cors')
+
+// Initialize app
+const app = express()
+
+// Use middlewares
+app.use(cors())
+app.use(express.json())
+
+app.post("/users", (req, res) => {
+    console.log(req.body)
+    res.json({message: "Post method success"})
 })
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+}) */
